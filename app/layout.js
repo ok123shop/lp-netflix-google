ca45f204fede66d2d35a01a480481a51f382b3ad
lqjava <545209147@qq.com>
2024-10-12 12:53:12 +0800

    1231

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import Header from "@/components/Header";
 import Body from "@/components/Body";
 
 export const metadata = {
-  title: "奈飞中文站 - 奈飞智能合租平台 稳定靠谱的合租平台",
-  description: "奈飞中文站 奈飞合租 奈飞拼车 奈飞共享账号",
+  title: "环球流媒体 - 奈飞智能合租平台 稳定靠谱的合租平台",
+  description: "环球流媒体 奈飞合租 奈飞拼车 奈飞共享账号",
 };
 
 export default function RootLayout({ children }) {
